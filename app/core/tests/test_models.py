7b5245ea8edc3102b79834748156272c14b50769
magda <magda@soft.co.za>
2020-10-20 15:53:33 +0200

    Fixed flake errors

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,13 +1,14 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successfull(self):
         """test creating a new user with an email is successful"""
-        email="test@email.com"
-        password="Testpass123"
-        user=get_user_model().objects.create_user(
+        email = "test@email.com"
+        password = "Testpass123"
+        user = get_user_model().objects.create_user(
             email=email,
             password=password
         )
@@ -15,23 +16,20 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
-
     def test_new_user_email_normalized(self):
         """Test the email for a new user is normalized"""
-        email="test@LONDONAPPDEV.COM"
-        user=get_user_model().objects.create_user(email, 'test123')
+        email = "test@LONDONAPPDEV.COM"
+        user = get_user_model().objects.create_user(email, 'test123')
         self.assertEqual(user.email, email.lower())
-
 
     def new_user_invalid_email(self):
         """Test creating email with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, "test123")
 
-
     def test_create_new_superuser(self):
         """ Teat creating new superuser"""
-        user=get_user_model().objects.create_superuser(
+        user = get_user_model().objects.create_superuser(
             "test@email.com",
             "test123"
         )
