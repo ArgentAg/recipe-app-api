3598c47a4f21e4608f41de13ac24143ec69475a7
magda <magda@soft.co.za>
2020-10-20 12:26:06 +0200

    Configured postgres db.

--- app/core/models.py
+++ app/core/models.py
@@ -15,7 +15,6 @@ class UserManager(BaseUserManager):
 
         return user
 
-
     def create_superuser(self, email, password):
         """Create and saves new superuser"""
         user = self.create_user(email, password)
@@ -32,7 +31,5 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-
     objects = UserManager()
-
     USERNAME_FIELD = 'email'
--- app/tests.py
+++ app/tests.py
@@ -1,12 +1,11 @@
 from django.test import TestCase
 from calc import add, subtract
 
-class CalcTests(TestCase):
 
+class CalcTests(TestCase):
     def test_add_numbers(self):
         """ Test that two nembers are added together """
         self.assertEqual(add(3, 8), 11)
-
 
     def test_subtract_numbers(self):
         """ Test that values are subtracted and returned"""
